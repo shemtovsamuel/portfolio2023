cb487a136ed77dc43595797b358074d8f7b22753
Samuel Shemtov <samuel.shemtov@epitech.eu>
2023-08-07 19:33:10 +0200

    fix: desactiv language

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -8,32 +8,32 @@ import LanguageContext from "../components/LanguageContext";
 import Translate from "./Translate";
 
 const Layout = ({ children, blog }) => {
-    const [night, setNight] = useState(true);
-    const [language, setLanguage] = useState("en");
+  const [night, setNight] = useState(true);
+  const [language, setLanguage] = useState("en");
 
-    const toggleNight = () => {
-        document.querySelector("body").classList.toggle("theme-light");
-        setNight(!night);
-    };
+  const toggleNight = () => {
+    document.querySelector("body").classList.toggle("theme-light");
+    setNight(!night);
+  };
 
-    const toggleLanguage = () => {
-        setLanguage(language === "en" ? "fr" : "en");
-    };
+  const toggleLanguage = () => {
+    setLanguage(language === "en" ? "fr" : "en");
+  };
 
-    return (
-        <ThemeContext.Provider value={{ night, toggleNight }}>
-            <LanguageContext.Provider value={{ language, toggleLanguage }}>
-                <Fragment>
-                    <ImageView />
-                    <Header blog={blog} />
-                    {children}
-                    <DayNightMood />
-                    <Translate />
-                    {blog && <BackBtn />}
-                </Fragment>
-            </LanguageContext.Provider>
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ night, toggleNight }}>
+      {/* <LanguageContext.Provider value={{ language, toggleLanguage }}> */}
+      <Fragment>
+        <ImageView />
+        <Header blog={blog} />
+        {children}
+        <DayNightMood />
+        {/* <Translate /> */}
+        {blog && <BackBtn />}
+      </Fragment>
+      {/* </LanguageContext.Provider> */}
+    </ThemeContext.Provider>
+  );
 };
 
 export default Layout;
